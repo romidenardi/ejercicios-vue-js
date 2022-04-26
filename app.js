eb136616a1511f079a01453e14fbcad4ae023b70
romidenardi <rominadenardi@gmail.com>
2022-04-25 22:22:04 -0300

    main commit 5

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ var app = new Vue({
         welcomeMessage: "¡Hola, Mundo!",
         a: 5,
         b: 3,
+        subtitle: "<p>Jugando con Vue</p>",
+        photo: "./images/london.jpg",
+        altText: "London",
+        defaultText: "Best perro ever"
     },
     methods: {
         calculatorMethod() {
-                return this.a * this.b
-            }
+            return this.a * this.b;
+        },
+        calculatorDirective() {
+            console.log(this.calculatorComputed);
+        }
     },
     computed: {
         calculatorComputed() {
-            return this.a * this.b
+            return this.a * this.b;
         }
     }
 })
