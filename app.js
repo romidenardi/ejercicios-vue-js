203e7c8530239ee3c1c98cefdb00f56bb2c1c32b
romidenardi <rominadenardi@gmail.com>
2022-04-27 22:32:59 -0300

    ejercicios clase 3

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = new Vue({
+
     el: "#app",
+
     data: {
         welcomeMessage: "¡Hola, Mundo!",
         a: 5,
@@ -7,19 +9,44 @@ var app = new Vue({
         subtitle: "<p>Jugando con Vue</p>",
         photo: "./images/london.jpg",
         altText: "London",
-        defaultText: "Best perro ever"
+        defaultText: "Best perro ever",
+        showGreeting: true,
+        showConditional: "5",
+        buttonText: "Ocultar texto",
+        showMessage: true,
+        products: [
+            {id: 1, name: "Avioncito", productPhoto: "./images/avioncito.png"},
+            {id: 2, name: "Mono", productPhoto: "./images/mono-de-peluche.png"},
+            {id: 3, name: "Chiches", productPhoto: "./images/set-chiches-agua.png"},
+        ],
+        ccsStyle:{
+            "classOne": true,
+            "classTwo": true,
+        }
     },
+
     methods: {
         calculatorMethod() {
             return this.a * this.b;
         },
         calculatorDirective() {
             console.log(this.calculatorComputed);
+        },
+        changeButton() {
+            this.showMessage = !this.showMessage;
+            if (this.showMessage) this.buttonText = "Ocultar texto";
+            else this.buttonText = "Mostrar texto";
+        },
+        classForTable(i) {
+            if (i % 2 == 0) return "darkBackground";
+            else return "lightBackground";
         }
     },
+
     computed: {
         calculatorComputed() {
             return this.a * this.b;
         }
-    }
+    },
+
 })
