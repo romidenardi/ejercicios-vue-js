208db11fddb9f60ee961c3a7c6121c0da253c6c8
romidenardi <rominadenardi@gmail.com>
2022-05-02 22:43:10 -0300

    ejercicios clase 4

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
+Vue.component ("my-component", { 
+    template:
+        `<div>
+            <h1>¡Hola, Coders!</h1>
+            <p> Estamos probando componentes </p>
+        </div>`
+});
+
+Vue.component ("product-gride", {
+    
+    props: {
+        name: {type: String, required: true},
+        id: {type: String, required: true},
+        productPhoto: {type: String, required: true},
+    },
+    
+    template:
+    `<div>
+        <ul>
+            <li class="product">
+                <span> {{name}} </span>
+                <span> {{id}}  </span>
+                <span> <img :src="productPhoto" :alt="name" :style="{'width':'30px'}"/> </span>
+            </li>
+        </ul>
+    </div>`
+
+});
+
 var app = new Vue({
 
     el: "#app",
+    
+    // El template se puede poner acá o en el HTML (como tag)
+
+    template: `<div>
+            <my-component> </my-component>
+            </br>
+            <h1>A mano</h1>
+            <product-gride 
+                name="London" 
+                id="000" 
+                product-photo="./images/london.jpg"> 
+            </product-gride>
+            </br>
+            <h1>V For</h1>
+            <product-gride v-for="(item, i) in products" :key="i"
+                :name="item.name"
+                :id="item.id"
+                :product-photo="item.productPhoto" :alt="item.name">
+            </product-gride>
+        </div>`,
 
     data: {
         welcomeMessage: "¡Hola, Mundo!",
@@ -15,9 +64,9 @@ var app = new Vue({
         buttonText: "Ocultar texto",
         showMessage: true,
         products: [
-            {id: 1, name: "Avioncito", productPhoto: "./images/avioncito.png"},
-            {id: 2, name: "Mono", productPhoto: "./images/mono-de-peluche.png"},
-            {id: 3, name: "Chiches", productPhoto: "./images/set-chiches-agua.png"},
+            {id: "001", name: "Avioncito", productPhoto: "./images/avioncito.png"},
+            {id: "002", name: "Mono", productPhoto: "./images/mono-de-peluche.png"},
+            {id: "003", name: "Chiches", productPhoto: "./images/set-chiches-agua.png"},
         ],
         ccsStyle:{
             "classOne": true,
